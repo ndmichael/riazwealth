6db1899411206826371641559d41bccffe3b9756
ndmichael <ukejemicheal@gmail.com>
2024-10-06 09:45:03 +0100

    views for each pages

--- pages/views.py
+++ pages/views.py
@@ -7,34 +7,34 @@ def index(request):
     context = {
         'title': 'Wealth Wheel Capital Investment'
     }
-    return render(request, "investments/index.html", context)
+    return render(request, "pages/index.html", context)
 
 def about(request):
     context = {
         'title': 'About Us'
     }
-    return render(request, 'investments/about.html', context)
+    return render(request, 'pages/about.html', context)
 
 def faqs(request):
     context = {
         'title': 'FAQs'
     }
-    return render(request, 'investments/faqs.html', context)
+    return render(request, 'pages/faqs.html', context)
 
 def offers(request):
     context = {
         'title': 'Investment Offers'
     }
-    return render(request, 'investments/offers.html', context)
+    return render(request, 'pages/offers.html', context)
 
 def partnerships(request):
     context = {
         'title': 'Partnerships and Loan'
     }
-    return render(request, 'investments/partnerships.html', context)
+    return render(request, 'pages/partnerships.html', context)
 
 def contact(request):
     context = {
         'title': 'Contact Us'
     }
-    return render(request, 'investments/contact.html', context)
+    return render(request, 'pages/contact.html', context)
