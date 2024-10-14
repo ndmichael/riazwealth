e0a3f875a29420b387db5c6391a9ffb04fd998b7
ndmichael <ukejemicheal@gmail.com>
2024-10-14 15:14:55 +0100

    add a redirected view if is_staff or not

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+@login_required
+def user_dashboard(request):
+    if request.user.is_staff:
+        return redirect('admindashboard')
+    return redirect('clientdashboard', username=request.user.username)
