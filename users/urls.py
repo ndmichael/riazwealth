9be9685f4b3eedb3e766bafc8ee558f96401af91
ndmichael <ukejemicheal@gmail.com>
2024-10-16 17:15:46 +0100

    clients dashboard url added

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import (
-    user_dashboard
+    user_dashboard,
+    client_dashboard,
 )
 
 
 
 
 urlpatterns = [
-    # path('dashboard/', profile , name="clientdashboard"),
+    path('dashboard', client_dashboard , name="clientdashboard"),
     path('', user_dashboard, name="user_dashboard"),
 ]
