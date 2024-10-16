3f0822578957fdf4cdecda6815a5c0fb20c51a7c
ndmichael <ukejemicheal@gmail.com>
2024-10-16 15:52:44 +0100

    Customize the CustomUser fields in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 from accounts.models import CustomUser, Profile
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-admin.site.register(CustomUser)
+# admin.site.register(CustomUser)
 admin.site.register(Profile)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    search_fields = ['username', 'first_name', 'last_name',]
+    list_display = ('username', 'first_name', 'last_name',)
+    filter_horizontal = ('groups', 'user_permissions')
+    fieldsets = (
+        ('Usuario', {'fields': ('username', 'password')}),
+        ('Personal Informations', {'fields': (
+            'first_name',
+            'last_name',
+            'email',
+        )}),
+        ('Permissions', {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'groups',
+            'user_permissions'
+        )}),
+    )
+
+# admin.site.register(User, UserAdmin)
