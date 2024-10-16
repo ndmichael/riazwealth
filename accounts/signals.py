fe10829ac72a94f6d632fce1e3ca7d49ed602c6d
ndmichael <ukejemicheal@gmail.com>
2024-10-16 15:34:47 +0100

    Allow profile for only clients

--- accounts/signals.py
+++ accounts/signals.py
@@ -7,11 +7,12 @@ from .models import CustomUser, Profile
 def create_user_profile(sender, instance, created, **kwargs):
     print("sender ", sender)
     print("created: ", created)
-    if created:
+    if created and not instance.is_staff and not instance.is_superuser:
         profile = Profile.objects.create(user=instance)
         profile.referral_code = str(uuid4()).replace('-','').upper()[:8]
         profile.save()
 
 @receiver(post_save, sender=CustomUser)
 def save_profile(sender, instance, **kwargs):
-    instance.profiles.save()
+    if hasattr(instance, 'profile'):
+        instance.profiles.save()
