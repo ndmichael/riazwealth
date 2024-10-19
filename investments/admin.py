f0e452350a61f3c65b1d2b38c54cd9753b828a58
ndmichael <ukejemicheal@gmail.com>
2024-10-19 11:04:02 +0100

    add plans models to admin

--- investments/admin.py
+++ investments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import InvestmentPlan, UserInvestment
 
-# Register your models here.
+
+
+
+admin.site.register(InvestmentPlan)
+admin.site.register(UserInvestment)
+
