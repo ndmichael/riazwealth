7294e34ebd234cd57021c93ba9a261a96f58cd0a
msani555 <mahmudribadu3@gmail.com>
2024-10-30 14:03:56 +0100

    Form passed to investment views

--- investments/views.py
+++ investments/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import MakePaymentForm, ConfirmPaymentForm
 
-# Create your views here.
+
+
+def buy_investment(request, package: str):
+    paymentForm = MakePaymentForm(request.POST)
+    confirmForm = ConfirmPaymentForm(request.POST)
+    context = {
+        "title": "Buy investment",
+        "cp_form": confirmForm,
+        "p_form" : paymentForm
+    }
+    return render(request, "investments/buy_investment.html", context)
