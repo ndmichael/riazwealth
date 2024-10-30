2645b0a8c4b6ad978732f7fdafcdc39319d9213f
ndmichael <ukejemicheal@gmail.com>
2024-10-30 15:22:52 +0100

    set payment to min

--- investments/forms.py
+++ investments/forms.py
@@ -9,7 +9,11 @@ payment_method = (
     ('usdt', 'USDT')
 )
 class MakePaymentForm(forms.Form):
-    amount = forms.DecimalField(max_digits=10, decimal_places=2, widget=forms.NumberInput({'class': 'form-control-lg'}))
+    amount = forms.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        widget=forms.NumberInput({'class': 'form-control-lg', 'min':100})
+    )
     payment_type = forms.ChoiceField(choices=payment_method, widget=forms.Select({'class': 'form-select form-select-lg'}))
 
 
