0fc2b23b766a1c919375b72c1487733dd8b7d14b
ndmichael <ukejemicheal@gmail.com>
2024-10-30 16:08:24 +0100

    Workedon the investment payment page

--- investments/views.py
+++ investments/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
+from .models import InvestmentPlan
 from .forms import MakePaymentForm, ConfirmPaymentForm
 
 
 
-def buy_investment(request, package: str):
-    paymentForm = MakePaymentForm(request.POST)
-    confirmForm = ConfirmPaymentForm(request.POST)
+def buy_investment(request, plan: str):
+    plan = InvestmentPlan.objects.get(name=plan)
+    if request.method == "POST":
+        paymentForm = MakePaymentForm(request.POST)
+        confirmForm = ConfirmPaymentForm(request.POST)
+        if paymentForm.is_valid() and confirmForm.is_valid():
+            pass
+    else:
+        paymentForm = MakePaymentForm()
+        confirmForm = ConfirmPaymentForm()
+
     context = {
         "title": "Buy investment",
+        "plan": plan,
         "cp_form": confirmForm,
         "p_form" : paymentForm
     }
