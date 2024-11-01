5370cbd9670280f62b3d125d59beae0e1193406b
ndmichael <ukejemicheal@gmail.com>
2024-11-01 14:53:59 +0100

    added referral code validators

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from crispy_forms.layout import Layout, Submit, Row, Field
 from crispy_forms.helper import FormHelper
 from crispy_bootstrap5.bootstrap5 import FloatingField
+from .models import CustomUser
 
 
 class MyCustomSignupForm(SignupForm):
@@ -14,10 +15,9 @@ class MyCustomSignupForm(SignupForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__( *args, **kwargs)
-        self.fields['password1'].help_text = ""
-        # self.fields['upliner'].disabled = True
 
-        # super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text = ""
+
         self.helper = FormHelper()
         self.helper.disable_csrf = True
         self.helper.form_tag = False
@@ -41,3 +41,22 @@ class MyCustomSignupForm(SignupForm):
                 Field("terms_and_condition", wrapper_class='col-md-6', css_class="form-check-input"),
             )                      
         )
+    
+    def clean_referral_code(self):
+        referral_code = self.cleaned_data.get("referral_code")
+        if referral_code and not CustomUser.objects.filter(username=referral_code.upper()).exists():
+            raise forms.ValidationError("Invalid referral code.")
+        return referral_code
+    
+    # def save(self, request):
+    #     # Call the parent save to create the user
+    #     user = super().save(request)
+
+    #     # If there's a referral code, create a referral record
+    #     referral_code = self.cleaned_data.get("referral_code")
+    #     if referral_code:
+    #         referrer = CustomUser.objects.get(username=referral_code)
+    #         Referral.objects.create(referred_by=referrer, referred_to=user)
+
+    #     return user
+    
