ae8689b889c528f243c9295625ad2a74bdd33926
ndmichael <ukejemicheal@gmail.com>
2024-11-01 14:59:51 +0100

    New model added

--- referrals/models.py
+++ referrals/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+
+
+class Referral(models.Model):
+    referred_by = models.ForeignKey(CustomUser, related_name='referrer', on_delete=models.CASCADE)
+    referred_to = models.ForeignKey(CustomUser, related_name='referred_user', on_delete=models.CASCADE)
+    referral_date = models.DateTimeField(auto_now_add=True)
+    referral_bonus = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.referred_by.username} referred {self.referred_to.username}"
