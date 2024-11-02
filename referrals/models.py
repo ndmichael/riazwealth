20baa1a55de60deb23092f806bb48d336759cdb1
ndmichael <ukejemicheal@gmail.com>
2024-11-02 12:15:26 +0100

    Added 2 new fields

--- referrals/models.py
+++ referrals/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from accounts.models import CustomUser
 
 
@@ -6,8 +7,9 @@ from accounts.models import CustomUser
 class Referral(models.Model):
     referred_by = models.ForeignKey(CustomUser, related_name='referrer', on_delete=models.CASCADE)
     referred_to = models.ForeignKey(CustomUser, related_name='referred_user', on_delete=models.CASCADE)
-    referral_date = models.DateTimeField(auto_now_add=True)
     referral_bonus = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    created_at = models.DateTimeField(default=timezone.now)
+    used_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.referred_by.username} referred {self.referred_to.username}"
