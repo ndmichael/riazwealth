c0a5dbd2ae523806563065ffb39229a6269d49e3
ndmichael <ukejemicheal@gmail.com>
2024-11-06 16:00:06 +0100

    adjsuted form with css

--- withdrawals/forms.py
+++ withdrawals/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import WithdrawalRequest
+from crispy_forms.layout import Layout, Submit, Row
+from crispy_forms.helper import FormHelper
+from crispy_bootstrap5.bootstrap5 import FloatingField
 
 class WithdrawalRequestForm(forms.ModelForm):
     class Meta:
@@ -10,3 +13,18 @@ class WithdrawalRequestForm(forms.ModelForm):
         amount = self.cleaned_data.get('amount')
         # Add your validation logic here as before
         return amount
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+        # super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                FloatingField("amount", wrapper_class='col-12', css_class="row-fluid"),
+            ),
+             Row(
+                FloatingField("payment_option", wrapper_class='col-12', css_class="row-fluid"),
+            ),
+            Submit('submit', 'REQUEST WITHDRAW', css_class="col-12 btn-lg btn-1")
+        )
