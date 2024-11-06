ff57f8c3a9c4bff0c179f1b54ef10da391aa8dca
ndmichael <ukejemicheal@gmail.com>
2024-11-06 14:58:07 +0100

    return withdrawals form

--- withdrawals/views.py
+++ withdrawals/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import WithdrawalRequestForm
 
-# Create your views here.
+def request_withdrawal(request):
+    if request.method == 'POST':
+        form = WithdrawalRequestForm(request.POST)
+        if form.is_valid():
+            withdrawal_request = form.save(commit=False)
+            withdrawal_request.user = request.user  # Associate the request with the logged-in user
+            withdrawal_request.save()
+            return redirect('withdrawal_success')  # Redirect to a success page or another view
+    else:
+        form = WithdrawalRequestForm()
+
+    return render(request, 'withdrawal/request_withdrawal.html', {'form': form})
