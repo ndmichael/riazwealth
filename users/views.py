c9520c419e6d603084b479bfd62b98077a0f5225
ndmichael <ukejemicheal@gmail.com>
2024-11-06 14:53:53 +0100

    return withdrawals form in the user view

--- users/views.py
+++ users/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from investments.models import InvestmentPlan, UserInvestment
+from investments.models import InvestmentPlan
+from withdrawals.forms  import WithdrawalRequestForm
 
 # Create your views here.
 
 @login_required
 def client_dashboard(request):
     plans = InvestmentPlan.objects.all()
+    form = WithdrawalRequestForm()
     context ={
         "title": "client dashboard",
-        "plans": plans
+        "plans": plans,
+        "form": form
     }
     return render(request, "users/client_dashboard.html", context)
 
