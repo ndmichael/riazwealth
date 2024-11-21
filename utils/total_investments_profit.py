8a37cc602f25de322e31d519a7bed3c1d9a6ded6
ndmichael <ukejemicheal@gmail.com>
2024-11-21 16:33:35 +0100

    calculate total referrals bonuses"

--- utils/total_investments_profit.py
+++ utils/total_investments_profit.py
@@ -1,6 +1,12 @@
 from investments.models import UserInvestment
+from referrals.models import Referral
 
 def get_total_profits(user):
     investments = UserInvestment.objects.filter(user=user)
     total_profits = sum([investment.total_profit for investment in investments])
     return total_profits
+
+def get_total_referral_bonus(user):
+    referrals = Referral.objects.filter(referred_by=user)
+    total_bonuses = sum([referral.total_profit for referral in referrals])
+    return total_bonuses
