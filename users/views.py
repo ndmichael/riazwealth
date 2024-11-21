9b910200fd90e687b90f643c07cb451baca4db31
ndmichael <ukejemicheal@gmail.com>
2024-11-21 15:34:48 +0100

    return logic to template

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from investments.models import InvestmentPlan
+from investments.models import InvestmentPlan, UserInvestment
 from withdrawals.models import WithdrawalRequest
 from withdrawals.forms  import WithdrawalRequestForm
 
@@ -9,15 +9,26 @@ from withdrawals.forms  import WithdrawalRequestForm
 @login_required
 def client_dashboard(request):
     user = request.user
-    withdrawals = WithdrawalRequest.objects.all()
+    withdrawals = WithdrawalRequest.objects.filter(user=user)
     plans = InvestmentPlan.objects.all()
+    user_investments = UserInvestment.objects.filter(user=user)
+
+    withdrawals_amount = withdrawals.count()
+    total_investments = user_investments.count()
+    total_plans = plans.count()
+
+
     form = WithdrawalRequestForm()
     context ={
         "title": "client dashboard",
         "user": user,
         "plans": plans,
         "form": form,
-        "withdrawals" : withdrawals
+        "withdrawals" : withdrawals,
+        "user_investments": user_investments,
+        "withdrawals_amount": withdrawals_amount,
+        "total_investments": total_investments,
+        "total_plans": total_plans
     }
     return render(request, "users/client_dashboard.html", context)
 
