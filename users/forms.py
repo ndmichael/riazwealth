93c72fd72e526145b5b02669efdedb35fb0fa35c
ndmichael <ukejemicheal@gmail.com>
2024-11-25 13:37:44 +0100

    bootstrap5 floating form

--- users/forms.py
+++ users/forms.py
@@ -13,9 +13,36 @@ class UserForm(forms.ModelForm):
         model = CustomUser
         fields = ["username", "email", "first_name", "last_name"]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.disable_csrf = True
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Row(
+                FloatingField("username", wrapper_class='col-md-6'),
+                FloatingField("email", wrapper_class='col-md-6'),
+                FloatingField("first_name", wrapper_class='col-md-6'),
+                FloatingField("last_name", wrapper_class='col-md-6'),
+            ),
+        )
+
 
 class ProfileForm(forms.ModelForm):
 
     class Meta:
         model = Profile
         fields = ["bitcoin_wallet", "ethereum_wallet", "usdt_wallet"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.disable_csrf = True
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Row(
+                FloatingField("bitcoin_wallet", wrapper_class='col-md-12'),
+                FloatingField("ethereum_wallet", wrapper_class='col-md-12'),
+                FloatingField("usdt_wallet", wrapper_class='col-md-12'),
+            ),
+        )
