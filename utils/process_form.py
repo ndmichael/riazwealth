cfe1f3682f3518e156c071c7420f77f683aaf211
ndmichael <ukejemicheal@gmail.com>
2024-11-25 13:20:12 +0100

    logic to handle user and profile form

--- utils/process_form.py
+++ utils/process_form.py
@@ -1,19 +1,22 @@
 from django.contrib import messages
-from users.forms import UserForm
+from users.forms import UserForm, ProfileForm
 from django.shortcuts import get_object_or_404
 from accounts.models import Profile
 
 
-def handle_user_form(request):
+def handle_user_profile_form(request):
     user = request.user
-    profile = Profile.objects.get_or_create(user=user)
+    profile, created = Profile.objects.get_or_create(user=user)
 
     user_form = UserForm(instance=user)
-    
+    profile_form = ProfileForm(instance=profile)
     if request.method == "POST":
-        if user_form.is_valid():
+        user_form = UserForm(request.POST, instance=user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if user_form.is_valid() and profile_form.is_valid():
             # Save the form
             user_form.save()
+            profile_form.save()
             messages.success(request, "Your withdrawal request was submitted successfully!")
 
-    return user_form
+    return user_form, profile_form
