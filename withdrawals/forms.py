247d6e6f6b916aed3d6b141e9fd84863f508c4c8
ndmichael <ukejemicheal@gmail.com>
2024-12-05 14:31:36 +0100

    changed amount to dorpdown

--- withdrawals/forms.py
+++ withdrawals/forms.py
@@ -5,6 +5,22 @@ from crispy_forms.helper import FormHelper
 from crispy_bootstrap5.bootstrap5 import FloatingField
 
 class WithdrawalRequestForm(forms.ModelForm):
+    AMOUNT_CHOICES = [
+        (10, '$10'),
+        (20, '$20'),
+        (50, '$50'),
+        (100, '$100'),
+        (500, '$500'),
+        (1000, '$1,000'),
+        (2000, '$2,000'),
+        (5000, '$5,000'),
+        (10000, '$10,000')
+    ]
+
+    amount = forms.ChoiceField(
+        choices=AMOUNT_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
     class Meta:
         model = WithdrawalRequest
         fields = ['amount', 'payment_option']  # Include payment_option in the form
