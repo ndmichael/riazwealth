b9e213cfa71bcec7045326e0d3cb50c50b5c4cfe
ndmichael <ukejemicheal@gmail.com>
2025-01-09 19:40:43 +0100

    return all plans to the admin

--- admin_portal/views.py
+++ admin_portal/views.py
@@ -4,8 +4,10 @@ from investments.models import InvestmentPlan, UserInvestment
 
 def admin_dashboard(request):
     investments = UserInvestment.objects.all()
+    plans = InvestmentPlan.objects.all()
     context = {
         'investments': investments,
+        'plans': plans,
         'title': 'Admin Portal'
     }
     return render(request, "admin_portal/admin_dashboard.html", context)
