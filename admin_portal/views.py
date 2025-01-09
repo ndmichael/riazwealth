5f6a0199c111dbec8db3808b84bc397cb3bfa39a
ndmichael <ukejemicheal@gmail.com>
2025-01-09 16:42:47 +0100

    logic to return all users_investment

--- admin_portal/views.py
+++ admin_portal/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from investments.models import InvestmentPlan, UserInvestment
 
 
 def admin_dashboard(request):
+    investments = UserInvestment.objects.all()
     context = {
+        'investments': investments,
         'title': 'Admin Portal'
     }
     return render(request, "admin_portal/admin_dashboard.html", context)
