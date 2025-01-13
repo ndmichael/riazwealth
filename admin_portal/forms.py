a7b1c5aab2ed0ed97cf6c8956b2caca2ae1a2c49
ndmichael <ukejemicheal@gmail.com>
2025-01-13 17:37:34 +0100

    investments search forms

---
+++ admin_portal/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from investments.models import UserInvestment
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Div, Row
+from crispy_bootstrap5.bootstrap5 import FloatingField
+
+
+class InvestmentFilterForm(forms.Form):
+    """Form to filter complaint records."""
+    STATUS  =( 
+        ('', '----'),
+        (True, 'ACTIVE'),
+        (False, 'PENDING')
+    )
+    ref_token = forms.CharField(required=False)
+    status = forms.ChoiceField(choices=STATUS, required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.disable_csrf = True
+        self.helper.layout = Layout(
+            Row(
+                FloatingField("ref_token", wrapper_class='col-md-4'),
+                FloatingField("status", wrapper_class='col-md-4'),
+                Div(
+                    Submit('submit', 'filter',  css_class="col-12 col-md-8 btn-lg btn-primary"), 
+                    css_class='col-md-4',        
+                )    
+            ),
+        )
