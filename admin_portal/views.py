7252cff8fdd8a71e22c67f281c6281f1a6f5e475
ndmichael <ukejemicheal@gmail.com>
2025-01-13 17:44:59 +0100

    called the view logic and handling of active tab on form submission

--- admin_portal/views.py
+++ admin_portal/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from investments.models import InvestmentPlan, UserInvestment
+from .forms import InvestmentFilterForm
+from utils.filter_form import filter_investments
 
 
 def admin_dashboard(request):
+
     investments = UserInvestment.objects.all()
     active_investments = UserInvestment.objects.filter(status=True)
     pending_investments = UserInvestment.objects.filter(status=False)
@@ -10,12 +13,30 @@ def admin_dashboard(request):
     total_active = active_investments.count()
     total_pending = pending_investments.count()
     plans = InvestmentPlan.objects.all()
+
+    active_tab = 'content-admin-investment'  # Default tab
+    filterForm =  InvestmentFilterForm(request.GET or None)
+    if filterForm.is_valid():
+        # Use cleaned data from the form
+        ref_token = filterForm.cleaned_data.get('ref_token')
+        status = filterForm.cleaned_data.get('status')
+        print(f"ref token = {ref_token}")
+        print(f"status = {status}")
+        
+        active_tab = request.GET.get('active_tab', 'content-admin-investment')
+        investments = filter_investments(ref_token, status)
+        
+
     context = {
         'investments': investments,
         'plans': plans,
         'title': 'Admin Portal',
         "total_investments": total_investments,
         'total_active': total_active,
-        'total_pending': total_pending
+        'total_pending': total_pending,
+
+        # forms
+        "filterForm": filterForm,
+        "active_tab": active_tab
     }
     return render(request, "admin_portal/admin_dashboard.html", context)
