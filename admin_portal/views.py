5012fd5a79fa1211662c6bc4b9a20eb03455d45d
ndmichael <ukejemicheal@gmail.com>
2025-01-13 14:26:27 +0100

    return totals of pending active and all investmeents

--- admin_portal/views.py
+++ admin_portal/views.py
@@ -4,10 +4,18 @@ from investments.models import InvestmentPlan, UserInvestment
 
 def admin_dashboard(request):
     investments = UserInvestment.objects.all()
+    active_investments = UserInvestment.objects.filter(status=True)
+    pending_investments = UserInvestment.objects.filter(status=False)
+    total_investments = investments.count()
+    total_active = active_investments.count()
+    total_pending = pending_investments.count()
     plans = InvestmentPlan.objects.all()
     context = {
         'investments': investments,
         'plans': plans,
-        'title': 'Admin Portal'
+        'title': 'Admin Portal',
+        "total_investments": total_investments,
+        'total_active': total_active,
+        'total_pending': total_pending
     }
     return render(request, "admin_portal/admin_dashboard.html", context)
