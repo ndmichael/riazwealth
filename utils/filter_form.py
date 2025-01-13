b8d94a208df25ca3923c8843a32b0b4dacdd9ad5
ndmichael <ukejemicheal@gmail.com>
2025-01-13 21:02:41 +0100

    Update the filter form logic

--- utils/filter_form.py
+++ utils/filter_form.py
@@ -4,11 +4,10 @@ from django.contrib.postgres.search import SearchVector
 
 def filter_investments(ref_token="", status=""):
     # Logic to filter complaints by reference id
-    if ref_token and ref_token != '':
-        investments = UserInvestment.objects.annotate(search=SearchVector('reference_id'),).filter(search__icontains=ref_token)
-
-    # Logic to filter complaints by status
-    if status and status != '':
-        investments = UserInvestment.objects.annotate(search=SearchVector('status'),).filter(search__icontains=status)
+    investments = UserInvestment.objects.all()
+    if ref_token:
+        investments = UserInvestment.objects.filter(ref_token=ref_token)
+    if status:
+        investments = UserInvestment.objects.filter(status=status)
 
     return investments
