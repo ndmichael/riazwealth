64e2997ee6dda5d9111bc40c552ad8b397f01d42
ndmichael <ukejemicheal@gmail.com>
2025-01-14 03:26:38 +0100

    Adjusted logic to help fix issues to tab reload

--- admin_portal/views.py
+++ admin_portal/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Count, Q
 from investments.models import InvestmentPlan, UserInvestment
 from .forms import InvestmentFilterForm
 from utils.filter_form import filter_investments
@@ -7,16 +8,25 @@ from utils.filter_form import filter_investments
 def admin_dashboard(request):
 
     investments = UserInvestment.objects.all()
-    active_investments = UserInvestment.objects.filter(status=True)
-    pending_investments = UserInvestment.objects.filter(status=False)
-    total_investments = investments.count()
-    total_active = active_investments.count()
-    total_pending = pending_investments.count()
+    # active_investments = UserInvestment.objects.filter(status=True)
+    # pending_investments = UserInvestment.objects.filter(status=False)
+    # Aggregate counts in a single query
+    investment_counts = UserInvestment.objects.aggregate(
+        total_investments=Count('id'),
+        total_active=Count('id', filter=Q(status=True)),
+        total_pending=Count('id', filter=Q(status=False))
+    )
+    # total_investments = investments.count()
+    # total_active = active_investments.count()
+    # total_pending = pending_investments.count()
     plans = InvestmentPlan.objects.all()
 
     # active_tab = 'content-admin-investment'  # Default tab
     active_tab = request.GET.get('active_tab', 'content-admin-investment')
     filterForm =  InvestmentFilterForm(request.GET or None)
+
+    # --- Active Tab Handling ---
+    active_tab = request.GET.get('active_tab', active_tab)  
 
     if filterForm.is_valid():
         # Use cleaned data from the form
@@ -33,9 +43,10 @@ def admin_dashboard(request):
         'investments': investments,
         'plans': plans,
         'title': 'Admin Portal',
-        "total_investments": total_investments,
-        'total_active': total_active,
-        'total_pending': total_pending,
+        "total_investments": investment_counts['total_investments'],
+        'total_active': investment_counts['total_active'],
+        'total_pending': investment_counts['total_pending'],
+
 
         # forms
         "filterForm": filterForm,
