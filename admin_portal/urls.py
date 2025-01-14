e777383ad577894e5dbcb03c7690f63a66a430cb
ndmichael <ukejemicheal@gmail.com>
2025-01-14 23:12:05 +0100

    urls for modal content for investment details

--- admin_portal/urls.py
+++ admin_portal/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import admin_dashboard
+from .views import admin_dashboard, get_investment_details
 
 urlpatterns = [
     path('admin/dashboard', admin_dashboard, name='admindashboard'),
+    path('admin/modal-content/<int:pk>/', get_investment_details, name='get_investment_details'),
+    
 ]
