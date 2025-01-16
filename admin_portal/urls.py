135f03fb6e4484f41fa938664199dc2dd3079785
ndmichael <ukejemicheal@gmail.com>
2025-01-16 15:43:36 +0100

    URL fo0r toggle modal

--- admin_portal/urls.py
+++ admin_portal/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import admin_dashboard, get_investment_details
+from .views import admin_dashboard, get_investment_details, toggle_investment_status
 
 urlpatterns = [
     path('admin/dashboard', admin_dashboard, name='admindashboard'),
     path('admin/modal-content/<int:pk>/', get_investment_details, name='get_investment_details'),
+    path('admin/investments/<int:investment_id>/toggle-status/', toggle_investment_status, name='get_investment_details'),
     
 ]
