0605d83b7024d1a43bf959b12e6f15c362bfbd86
ndmichael <ukejemicheal@gmail.com>
2025-01-20 15:17:10 +0100

    Withdrawals adjusted added null temporary

--- withdrawals/models.py
+++ withdrawals/models.py
@@ -2,25 +2,31 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from investments.models import UserInvestment
 
+
+# Status of the withdrawal request
+STATUS_CHOICES = [
+    ('pending', 'Pending'),
+    ('approved', 'Approved'),
+    ('rejected', 'Rejected'),
+]
+PAYMENT_OPTIONS = [
+    ('ethereum', 'Ethereum'),
+    ('bitcoin', 'Bitcoin'),
+    ('usdt', 'USDT'),
+]
 class WithdrawalRequest(models.Model):
     # User who made the withdrawal request
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    investment = models.ForeignKey(
+        UserInvestment, 
+        on_delete=models.CASCADE, 
+        related_name='withdrawal_requests',
+    )  # Links to the specific investment
     
     # Amount requested for withdrawal
     amount = models.DecimalField(max_digits=15, decimal_places=2)
-    
-    # Status of the withdrawal request
-    STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('approved', 'Approved'),
-        ('rejected', 'Rejected'),
-    ]
-    PAYMENT_OPTIONS = [
-        ('ethereum', 'Ethereum'),
-        ('bitcoin', 'Bitcoin'),
-        ('usdt', 'USDT'),
-    ]
     payment_option = models.CharField(_("payment options"), max_length=10, choices=PAYMENT_OPTIONS, default="usdt")
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
     
@@ -35,4 +41,7 @@ class WithdrawalRequest(models.Model):
 
     def __str__(self):
         return f'{self.user.username} - {self.amount} ({self.status})'
+    
+
+
 
