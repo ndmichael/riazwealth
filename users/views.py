6b4a047b54a70b03ffe6c978167033fe37ebb7dd
ndmichael <ukejemicheal@gmail.com>
2025-01-21 14:32:41 +0100

    logic to handle withdrawal form

--- users/views.py
+++ users/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from investments.models import InvestmentPlan, UserInvestment
 from withdrawals.models import WithdrawalRequest
 from referrals.models import Referral
 from withdrawals.forms  import WithdrawalRequestForm
 from django.db.models import Sum, Q
+from decimal import Decimal
 
 from utils.total_investments_profit import get_user_total_profits, get_total_referral_bonus
 from utils.process_form import handle_user_profile_form
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 
@@ -31,10 +37,46 @@ def client_dashboard(request):
     total_referrals = referrals.count()
 
 
-
-    if request.POST:
+    if request.method == "POST":
+        # Create the form and pass the investments
         withdrawal_form = WithdrawalRequestForm(request.POST, investments=user_investments)
+        
+        if withdrawal_form.is_valid():
+            print(f"result: {withdrawal_form.cleaned_data}")
+
+            # Get the selected investment ID from the form's cleaned data
+            investment_id = withdrawal_form.cleaned_data['investment']
+            payment_option = withdrawal_form.cleaned_data['payment_option']
+            amount = withdrawal_form.cleaned_data['amount']
+
+            print(f"amount: {amount}")
+
+            
+            try:
+                # Fetch the actual UserInvestment instance corresponding to the selected ID
+                investment = UserInvestment.objects.get(id=investment_id)
+
+                WithdrawalRequest.objects.create(
+                    investment=investment, 
+                    amount=amount, 
+                    user=request.user, 
+                    payment_option=payment_option
+                )
+
+                # Show a success message
+                messages.success(request, "Withdrawal request submitted successfully!")
+                return redirect("clientdashboard")
+            
+            except UserInvestment.DoesNotExist:
+                messages.error(request, "Invalid investment selected.")
+                return redirect("clientdashboard")
+        
+        else:
+            # Print form errors for debugging
+            print("Form Errors:", withdrawal_form.errors)
+    
     else:
+        # If the request is GET, initialize the form with user investments
         withdrawal_form = WithdrawalRequestForm(investments=user_investments)
 
     user_form, profile_form = handle_user_profile_form(request)
