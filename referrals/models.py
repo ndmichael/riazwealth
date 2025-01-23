73d740ec2bc5ba4168b47a146135bb84a0c235d5
ndmichael <ukejemicheal@gmail.com>
2025-01-23 12:14:13 +0100

    adjusted field to boolean

--- referrals/models.py
+++ referrals/models.py
@@ -8,7 +8,7 @@ class Referral(models.Model):
     referred_by = models.ForeignKey(CustomUser, related_name='referrer', on_delete=models.CASCADE)
     referred_to = models.ForeignKey(CustomUser, related_name='referred_user', on_delete=models.CASCADE)
     bonus = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    bonus_status = models.CharField(choices=(("false", "False"), ("true", "True")), default="false")
+    bonus_status = models.BooleanField(default=False) 
     created_at = models.DateTimeField(default=timezone.now)
     used_at = models.DateTimeField(null=True, blank=True)
 
