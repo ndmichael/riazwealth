f8c8483085ff388e069b28dbf0b815c8cb892aa8
ndmichael <ukejemicheal@gmail.com>
2025-01-30 00:34:59 +0100

    urls for confirming the withdrawals

--- admin_portal/urls.py
+++ admin_portal/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import admin_dashboard, get_investment_details, toggle_investment_status
+from .views import (
+        admin_dashboard, 
+        get_investment_details, 
+        toggle_investment_status,
+        confirm_withdrawal
+)
 
 urlpatterns = [
     path('admin/dashboard', admin_dashboard, name='admindashboard'),
     path('admin/modal-content/<int:pk>/', get_investment_details, name='get_investment_details'),
     path('admin/investments/<int:investment_id>/toggle-status/', toggle_investment_status, name='get_investment_details'),
-    
+    path("admin/withdrawals/confirm/<int:withdrawal_id>/", confirm_withdrawal, name="confirm_withdrawal"),
 ]
