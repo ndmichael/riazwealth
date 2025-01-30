613c92ad80577d42476510d55812940ede054c6c
ndmichael <ukejemicheal@gmail.com>
2025-01-30 15:12:34 +0100

    added notifications to admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import GeneralNotification, UserNotification
 
-# Register your models here.
+
+admin.site.register(GeneralNotification)
+admin.site.register(UserNotification)
