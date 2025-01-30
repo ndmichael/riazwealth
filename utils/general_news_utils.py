afde71a1c851880036ee20c733cbead3bd25095f
ndmichael <ukejemicheal@gmail.com>
2025-01-30 16:23:18 +0100

    working on a utility function to handle general users from admin

---
+++ utils/general_news_utils.py
@@ -0,0 +1,13 @@
+from  notifications.forms import GeneralNewsForm
+from django.contrib import messages
+
+
+def post_general_news(request):
+    if request.method == 'POST':
+        form = GeneralNewsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "News posted successfully!")
+            return redirect("admin_dashboard")
+    else:
+        form = GeneralNewsForm()
