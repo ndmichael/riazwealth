09a4fe9f6bfb6b158f6d8d60e1bffad1371418d8
ndmichael <ukejemicheal@gmail.com>
2025-02-05 21:28:43 +0100

    urls for handling accrual profit

--- admin_portal/urls.py
+++ admin_portal/urls.py
@@ -3,7 +3,8 @@ from .views import (
         admin_dashboard, 
         get_investment_details, 
         toggle_investment_status,
-        confirm_withdrawal
+        confirm_withdrawal,
+        accrue_profits_for_all_users
 )
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('admin/modal-content/<int:pk>/', get_investment_details, name='get_investment_details'),
     path('admin/investments/<int:investment_id>/toggle-status/', toggle_investment_status, name='get_investment_details'),
     path("admin/withdrawals/confirm/<int:withdrawal_id>/", confirm_withdrawal, name="confirm_withdrawal"),
+    path('accrue_profits/', accrue_profits_for_all_users, name='accrue_profits_for_all_users'),
 ]
