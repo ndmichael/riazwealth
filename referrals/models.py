3325aaf04597506c04aea2e04eca33f3584547a5
ndmichael <ukejemicheal@gmail.com>
2025-02-06 17:17:22 +0100

    Logic to calculate bonus of referral

--- referrals/models.py
+++ referrals/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from accounts.models import CustomUser
+from decimal import Decimal
 
 
 
@@ -11,6 +12,13 @@ class Referral(models.Model):
     bonus_status = models.BooleanField(default=False) 
     created_at = models.DateTimeField(default=timezone.now)
     used_at = models.DateTimeField(null=True, blank=True)
+
+    def calculate_referral_bonus(self, amount):
+        """
+        Calculate the bonus for this referral based on the provided amount.
+        """
+        referral_bonus = amount * Decimal('0.05')
+        return referral_bonus
 
     def __str__(self):
         return f"{self.referred_by.username} referred {self.referred_to.username}"
