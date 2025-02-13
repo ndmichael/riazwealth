45d68c9e0fd8d870e6c7ba04b86ab8cfc1e2d394
ndmichael <ukejemicheal@gmail.com>
2025-02-13 12:54:48 +0100

    URLS to handle the view

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     user_dashboard,
     client_dashboard,
+    mark_as_read
 )
 
 
@@ -10,4 +11,5 @@ from .views import (
 urlpatterns = [
     path('dashboard', client_dashboard , name="clientdashboard"),
     path('', user_dashboard, name="user_dashboard"),
+    path('mark-as-read/', mark_as_read, name='mark_as_read'),
 ]
