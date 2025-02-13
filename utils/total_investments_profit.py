6a0d616d9a393314d9f7eccd5f7545d9ca3645a8
ndmichael <ukejemicheal@gmail.com>
2025-02-13 16:39:41 +0100

    Calculate the total invested amount

--- utils/total_investments_profit.py
+++ utils/total_investments_profit.py
@@ -10,3 +10,9 @@ def get_total_referral_bonus(user):
     referrals = Referral.objects.filter(referred_by=user)
     total_bonuses = sum([referral.bonus for referral in referrals])
     return total_bonuses
+
+def get_total_investment_amount(user):
+    investments = UserInvestment.objects.filter(user=user)
+    investment_amount = sum([investment.amount for investment in investments])
+    return investment_amount
+
