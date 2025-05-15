d34c99f2a45e4673a91df887d9fe2f59e8df2828
ndmichael <ukejemicheal@gmail.com>
2025-05-15 14:15:03 +0100

    updated signal

--- accounts/signals.py
+++ accounts/signals.py
@@ -3,16 +3,12 @@ from django.dispatch import receiver
 from uuid import uuid4
 from .models import CustomUser, Profile
 
-@receiver(post_save, sender=CustomUser)
-def create_user_profile(sender, instance, created, **kwargs):
-    print("sender ", sender)
-    print("created: ", created)
-    if created and not instance.is_staff and not instance.is_superuser:
-        profile = Profile.objects.create(user=instance)
-        profile.referral_code = str(uuid4()).replace('-','').upper()[:8]
-        profile.save()
 
 @receiver(post_save, sender=CustomUser)
-def save_profile(sender, instance, **kwargs):
-    if hasattr(instance, 'profile'):
-        instance.profiles.save()
+def create_user_profile(sender, instance, created, **kwargs):
+    if created and not instance.is_staff and not instance.is_superuser:
+        if not hasattr(instance, 'profiles'):
+            Profile.objects.create(
+                user=instance,
+                referral_code=str(uuid4()).replace('-', '').upper()[:8]
+            )
