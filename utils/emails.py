9631dfa48edaf43a7f5deacf743eac60b3e56c8b
ndmichael <ukejemicheal@gmail.com>
2026-02-09 16:39:41 +0100

    created an email utility

---
+++ utils/emails.py
@@ -0,0 +1,12 @@
+EMAIL_BRAND = {
+    "app_name": "Your App",
+    "primary_color": "#003366",   # header / titles
+    "accent_color": "#66A3FF",    # buttons / links
+    "success_color": "#32CD32",   # success states
+    "bg_soft": "#E9F7EA",         # subtle sections
+    "text_dark": "#0F172A",
+    "text_muted": "#475569",
+    "logo_url": "https://riazvest.com/static/images/logo.PNG",
+    "support_email": "support@yourdomain.com",
+    "app_url": "https://yourdomain.com",
+}
