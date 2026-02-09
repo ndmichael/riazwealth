3d7241c3285713c0dd5fee733eaf770efe80b729
ndmichael <ukejemicheal@gmail.com>
2026-02-09 21:57:15 +0100

    added a mail logic

--- utils/emails.py
+++ utils/emails.py
@@ -1,12 +1,14 @@
+from django.conf import settings
+
 EMAIL_BRAND = {
-    "app_name": "Your App",
+    "app_name": "Riazvest Secured",
     "primary_color": "#003366",   # header / titles
     "accent_color": "#66A3FF",    # buttons / links
     "success_color": "#32CD32",   # success states
     "bg_soft": "#E9F7EA",         # subtle sections
     "text_dark": "#0F172A",
     "text_muted": "#475569",
-    "logo_url": "https://riazvest.com/static/images/logo.PNG",
+    "logo_url": f"{settings.SITE_URL}/static/images/logo.PNG",
     "support_email": "support@yourdomain.com",
     "app_url": "https://yourdomain.com",
 }
