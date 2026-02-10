dcddd55fdb68dfb2f35b06d23696064ffa521667
ndmichael <ukejemicheal@gmail.com>
2026-02-10 17:03:52 +0100

    fixing bad rendering

--- accounts/adapters.py
+++ accounts/adapters.py
@@ -1,27 +1,27 @@
+# accounts/adapters.py
 from allauth.account.adapter import DefaultAccountAdapter
 from django.template.loader import render_to_string
-from django.utils.html import strip_tags
-import resend
-import os
+import resend, os
 
 resend.api_key = os.environ["RESEND_API_KEY"]
 
 class AccountAdapter(DefaultAccountAdapter):
     def send_mail(self, template_prefix, email, context):
-        subject = render_to_string(
-            f"{template_prefix}_subject.txt", context
-        ).strip()
+        # determine which template to send
+        if template_prefix.endswith("email_confirmation_signup"):
+            template = "account/email/email_confirmation_signup_message.html"
+            subject = render_to_string(
+                "account/email/email_confirmation_signup_subject.txt",
+                context,
+            ).strip()
+        else:
+            return  # or handle other mails later
 
-        html = render_to_string(
-            f"{template_prefix}_message.html", context
-        )
-
-        text = strip_tags(html)
+        html = render_to_string(template, context)
 
         resend.Emails.send({
             "from": "Riazvest <noreply@riazvest.com>",
             "to": email,
             "subject": subject,
             "html": html,
-            "text": text,
         })
