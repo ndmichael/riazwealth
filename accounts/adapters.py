507f2079f221af17bdfccfa52cd710cb5a53da8c
ndmichael <ukejemicheal@gmail.com>
2026-02-10 16:09:32 +0100

    updated adapter to use render api

--- accounts/adapters.py
+++ accounts/adapters.py
@@ -1,11 +1,27 @@
-# accounts/adapters.py
 from allauth.account.adapter import DefaultAccountAdapter
-import threading
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+import resend
+import os
+
+resend.api_key = os.environ["RESEND_API_KEY"]
 
 class AccountAdapter(DefaultAccountAdapter):
     def send_mail(self, template_prefix, email, context):
-        threading.Thread(
-            target=super().send_mail,
-            args=(template_prefix, email, context),
-            daemon=True,
-        ).start()
+        subject = render_to_string(
+            f"{template_prefix}_subject.txt", context
+        ).strip()
+
+        html = render_to_string(
+            f"{template_prefix}_message.html", context
+        )
+
+        text = strip_tags(html)
+
+        resend.Emails.send({
+            "from": "Riazvest <noreply@riazvest.com>",
+            "to": email,
+            "subject": subject,
+            "html": html,
+            "text": text,
+        })
