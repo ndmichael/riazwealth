3051edfabcb37028657ca9324713737536d6fcc6
ndmichael <ukejemicheal@gmail.com>
2026-02-16 16:13:23 +0100

    removed some no needed url

--- admin_portal/urls.py
+++ admin_portal/urls.py
@@ -3,14 +3,12 @@ from .views import (
         admin_dashboard, 
         get_investment_details, 
         toggle_investment_status,
-        confirm_withdrawal,
-        accrue_profits_for_all_users
+        accrue_profits_for_all_users,
 )
 
 urlpatterns = [
     path('admin/dashboard', admin_dashboard, name='admindashboard'),
     path('admin/modal-content/<int:pk>/', get_investment_details, name='get_investment_details'),
-    path('admin/investments/<int:investment_id>/toggle-status/', toggle_investment_status, name='get_investment_details'),
-    path("admin/withdrawals/confirm/<int:withdrawal_id>/", confirm_withdrawal, name="confirm_withdrawal"),
+    path('admin/investments/<int:investment_id>/toggle-status/', toggle_investment_status, name='toggle_investment_status'),
     path('accrue_profits/', accrue_profits_for_all_users, name='accrue_profits_for_all_users'),
 ]
