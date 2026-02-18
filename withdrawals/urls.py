07ffa70fe5bd9e90e775fa01920ef2a9af11b53d
ndmichael <ukejemicheal@gmail.com>
2026-02-18 01:36:07 +0100

    improved url

--- withdrawals/urls.py
+++ withdrawals/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (admin_create_withdrawal, confirm_withdrawal)
+from .views import (admin_create_withdrawal, confirm_withdrawal, admin_user_investments)
 
 urlpatterns = [
     path(
@@ -11,5 +11,9 @@ urlpatterns = [
         "admin/withdrawals/confirm/<int:withdrawal_id>/",
         confirm_withdrawal,
         name="confirm_withdrawal"
+    ),
+     path("admin/user-investments/<int:user_id>/", 
+          admin_user_investments, 
+          name="admin_user_investments"
     ),
 ]
