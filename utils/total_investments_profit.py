a904746e223a83c7ec08da2029d369a3602d3571
ndmichael <ukejemicheal@gmail.com>
2026-02-18 13:50:02 +0100

    logic to handle total package bonuses

--- utils/total_investments_profit.py
+++ utils/total_investments_profit.py
@@ -2,7 +2,11 @@ from investments.models import UserInvestment
 from referrals.models import Referral
 
 def get_user_total_profits(user):
-    investments = UserInvestment.objects.filter(user=user)
+    investments = UserInvestment.objects.filter(
+        user=user, 
+        status=True,
+        payment_verified=True
+    )
     total_profits = sum([investment.profit_accumulated for investment in investments])
     return total_profits
 
@@ -12,7 +16,20 @@ def get_total_referral_bonus(user):
     return total_bonuses
 
 def get_total_investment_amount(user):
-    investments = UserInvestment.objects.filter(user=user)
+    investments = UserInvestment.objects.filter(
+        user=user, 
+        status=True,
+        payment_verified=True
+    )
     investment_amount = sum([investment.amount for investment in investments])
     return investment_amount
+
+def get_total_package_bonus(user):
+    investments = UserInvestment.objects.filter(
+        user=user, 
+        status=True,
+        payment_verified=True
+    )
+    total_profits = sum([investment.profit_accumulated for investment in investments])
+    return total_profits
 
